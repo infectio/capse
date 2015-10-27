ea0ee1973d15dfe4ac54ea4a75370558fde4b27d
Artur Yakimovich <arthur.yakimovitch@gmail.com>
2015-10-27 20:28:09 +0100

    copyright updated

--- src/matlab/+caps/+particles/+pse/applyPSE.cpp
+++ src/matlab/+caps/+particles/+pse/applyPSE.cpp
@@ -98,4 +98,19 @@ void mexFunction(
         pseSum[i] = preSum;     // It's faster to sum up in an local variable and then transfer it 
     }  
 }
-    
+
+//     Infectio - a virus infection spread simulation platform
+//    Copyright (C) 2014-2015  Michael Schmidt, Artur Yakimovich, Yauhen Yakimovich
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//%     along with this program.  If not, see <http://www.gnu.org/licenses/>.  
--- src/matlab/remesh.m
+++ src/matlab/remesh.m
@@ -73,3 +73,20 @@ in_domain = intersect(intersect(find(partMat(:,1) > 0),find(partMat(:,1) < lengt
 partMat(in_domain,dim+1) = strength_at_gridPositions(:); % interpolated u_c at remeshed particles %originally dim+2
 %end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%     Infectio - a virus infection spread simulation platform
+%     Copyright (C) 2014-2015  Artur Yakimovich, Yauhen Yakimovich
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
